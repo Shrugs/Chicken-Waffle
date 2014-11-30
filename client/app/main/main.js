259e7b43930e7e1bf68657e36710eb273c4c747b
Matt Condon <mattgcondon@gmail.com>
2014-11-29 22:42:24 -0600

    init commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cpwApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
