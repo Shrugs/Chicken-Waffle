e381614b28c21ea119224742c37b6045abebdf27
Matt Condon <mattgcondon@gmail.com>
2014-12-02 00:20:14 -0600

    remove unused Team routes and function defs

--- server/api/Team/Team.controller.js
+++ server/api/Team/Team.controller.js
@@ -11,14 +11,14 @@ exports.index = function(req, res) {
   });
 };
 
-// Get a single Team
-exports.show = function(req, res) {
-  Team.findById(req.params.id, function (err, Team) {
-    if(err) { return handleError(res, err); }
-    if(!Team) { return res.send(404); }
-    return res.json(Team);
-  });
-};
+// // Get a single Team
+// exports.show = function(req, res) {
+//   Team.findById(req.params.id, function (err, Team) {
+//     if(err) { return handleError(res, err); }
+//     if(!Team) { return res.send(404); }
+//     return res.json(Team);
+//   });
+// };
 
 // Creates a new Team in the DB.
 exports.create = function(req, res) {
@@ -28,31 +28,31 @@ exports.create = function(req, res) {
   });
 };
 
-// Updates an existing Team in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Team.findById(req.params.id, function (err, Team) {
-    if (err) { return handleError(res, err); }
-    if(!Team) { return res.send(404); }
-    var updated = _.merge(Team, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, Team);
-    });
-  });
-};
+// // Updates an existing Team in the DB.
+// exports.update = function(req, res) {
+//   if(req.body._id) { delete req.body._id; }
+//   Team.findById(req.params.id, function (err, Team) {
+//     if (err) { return handleError(res, err); }
+//     if(!Team) { return res.send(404); }
+//     var updated = _.merge(Team, req.body);
+//     updated.save(function (err) {
+//       if (err) { return handleError(res, err); }
+//       return res.json(200, Team);
+//     });
+//   });
+// };
 
-// Deletes a Team from the DB.
-exports.destroy = function(req, res) {
-  Team.findById(req.params.id, function (err, Team) {
-    if(err) { return handleError(res, err); }
-    if(!Team) { return res.send(404); }
-    Team.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.send(204);
-    });
-  });
-};
+// // Deletes a Team from the DB.
+// exports.destroy = function(req, res) {
+//   Team.findById(req.params.id, function (err, Team) {
+//     if(err) { return handleError(res, err); }
+//     if(!Team) { return res.send(404); }
+//     Team.remove(function(err) {
+//       if(err) { return handleError(res, err); }
+//       return res.send(204);
+//     });
+//   });
+// };
 
 function handleError(res, err) {
   return res.send(500, err);
